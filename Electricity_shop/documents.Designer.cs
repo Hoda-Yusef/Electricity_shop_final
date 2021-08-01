88302083193ee41d611cbc5bee608fb22145b83b
Yusef Aborokon <yusefaborokon@gmail.com>
2021-08-01 20:35:07 +0300

    updated

--- Electricity_shop/documents.Designer.cs
+++ Electricity_shop/documents.Designer.cs
@@ -37,8 +37,13 @@ namespace Electricity_shop
             this.panel4 = new System.Windows.Forms.Panel();
             this.Lbl_inStockDocument = new System.Windows.Forms.Label();
             this.Lbl_info = new System.Windows.Forms.Label();
-            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker_from = new System.Windows.Forms.DateTimePicker();
             this.Btn_search = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker_to = new System.Windows.Forms.DateTimePicker();
+            this.Pic_createPDF = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_createPDF)).BeginInit();
             this.SuspendLayout();
             // 
             // Btn_exit
@@ -49,10 +54,10 @@ namespace Electricity_shop
             this.Btn_exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(113)))), ((int)(((byte)(184)))));
             this.Btn_exit.Image = ((System.Drawing.Image)(resources.GetObject("Btn_exit.Image")));
             this.Btn_exit.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.Btn_exit.Location = new System.Drawing.Point(1205, -3);
+            this.Btn_exit.Location = new System.Drawing.Point(912, 0);
             this.Btn_exit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Btn_exit.Name = "Btn_exit";
-            this.Btn_exit.Size = new System.Drawing.Size(34, 28);
+            this.Btn_exit.Size = new System.Drawing.Size(39, 37);
             this.Btn_exit.TabIndex = 38;
             this.Btn_exit.UseVisualStyleBackColor = false;
             this.Btn_exit.Click += new System.EventHandler(this.Btn_exit_Click);
@@ -60,10 +65,11 @@ namespace Electricity_shop
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(113)))), ((int)(((byte)(184)))));
+            this.panel1.Cursor = System.Windows.Forms.Cursors.SizeAll;
             this.panel1.Location = new System.Drawing.Point(-5, -2);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1257, 34);
+            this.panel1.Size = new System.Drawing.Size(963, 22);
             this.panel1.TabIndex = 39;
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
@@ -72,27 +78,28 @@ namespace Electricity_shop
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(113)))), ((int)(((byte)(184)))));
-            this.panel3.Location = new System.Drawing.Point(-2, 676);
+            this.panel3.Location = new System.Drawing.Point(-2, 753);
             this.panel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1245, 13);
+            this.panel3.Size = new System.Drawing.Size(954, 14);
             this.panel3.TabIndex = 41;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(113)))), ((int)(((byte)(184)))));
-            this.panel2.Location = new System.Drawing.Point(1234, -6);
+            this.panel2.Location = new System.Drawing.Point(944, -6);
             this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(10, 683);
+            this.panel2.Size = new System.Drawing.Size(10, 767);
             this.panel2.TabIndex = 40;
             // 
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(113)))), ((int)(((byte)(184)))));
-            this.panel4.Location = new System.Drawing.Point(-2, -4);
+            this.panel4.Location = new System.Drawing.Point(-2, -6);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(10, 691);
+            this.panel4.Size = new System.Drawing.Size(10, 769);
             this.panel4.TabIndex = 42;
             // 
             // Lbl_inStockDocument
@@ -100,11 +107,11 @@ namespace Electricity_shop
             this.Lbl_inStockDocument.AutoSize = true;
             this.Lbl_inStockDocument.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Lbl_inStockDocument.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Lbl_inStockDocument.Location = new System.Drawing.Point(538, 52);
+            this.Lbl_inStockDocument.Location = new System.Drawing.Point(393, 24);
             this.Lbl_inStockDocument.Name = "Lbl_inStockDocument";
-            this.Lbl_inStockDocument.Size = new System.Drawing.Size(199, 47);
+            this.Lbl_inStockDocument.Size = new System.Drawing.Size(160, 60);
             this.Lbl_inStockDocument.TabIndex = 43;
-            this.Lbl_inStockDocument.Text = "רווח והפסד";
+            this.Lbl_inStockDocument.Text = "דו\"חות";
             // 
             // Lbl_info
             // 
@@ -112,27 +119,27 @@ namespace Electricity_shop
             this.Lbl_info.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.Lbl_info.Font = new System.Drawing.Font("Stencil", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Lbl_info.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Lbl_info.Location = new System.Drawing.Point(26, 132);
+            this.Lbl_info.Location = new System.Drawing.Point(194, 156);
             this.Lbl_info.Name = "Lbl_info";
             this.Lbl_info.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Lbl_info.Size = new System.Drawing.Size(660, 531);
+            this.Lbl_info.Size = new System.Drawing.Size(581, 576);
             this.Lbl_info.TabIndex = 44;
             this.Lbl_info.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // dateTimePicker
+            // dateTimePicker_from
             // 
-            this.dateTimePicker.CalendarFont = new System.Drawing.Font("Showcard Gothic", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.dateTimePicker.CalendarForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.dateTimePicker.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
-            this.dateTimePicker.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
-            this.dateTimePicker.CalendarTitleForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.dateTimePicker.CustomFormat = "dd-MM-yyyy";
-            this.dateTimePicker.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker.Location = new System.Drawing.Point(820, 227);
-            this.dateTimePicker.Name = "dateTimePicker";
-            this.dateTimePicker.Size = new System.Drawing.Size(257, 34);
-            this.dateTimePicker.TabIndex = 45;
+            this.dateTimePicker_from.CalendarFont = new System.Drawing.Font("Showcard Gothic", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker_from.CalendarForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dateTimePicker_from.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.dateTimePicker_from.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.dateTimePicker_from.CalendarTitleForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.dateTimePicker_from.CustomFormat = "dd-MM-yyyy";
+            this.dateTimePicker_from.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker_from.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker_from.Location = new System.Drawing.Point(559, 109);
+            this.dateTimePicker_from.Name = "dateTimePicker_from";
+            this.dateTimePicker_from.Size = new System.Drawing.Size(176, 34);
+            this.dateTimePicker_from.TabIndex = 45;
             // 
             // Btn_search
             // 
@@ -142,30 +149,83 @@ namespace Electricity_shop
             this.Btn_search.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Btn_search.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.Btn_search.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.Btn_search.Location = new System.Drawing.Point(859, 374);
+            this.Btn_search.Location = new System.Drawing.Point(45, 97);
             this.Btn_search.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Btn_search.Name = "Btn_search";
-            this.Btn_search.Size = new System.Drawing.Size(181, 51);
+            this.Btn_search.Size = new System.Drawing.Size(117, 51);
             this.Btn_search.TabIndex = 46;
             this.Btn_search.Text = "חפש";
             this.Btn_search.UseVisualStyleBackColor = false;
             this.Btn_search.Click += new System.EventHandler(this.Btn_search_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(756, 106);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(109, 31);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "מתאריך :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(417, 109);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(126, 31);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "עד תאריך :";
+            // 
+            // dateTimePicker_to
+            // 
+            this.dateTimePicker_to.CalendarFont = new System.Drawing.Font("Showcard Gothic", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker_to.CalendarForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dateTimePicker_to.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.dateTimePicker_to.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.dateTimePicker_to.CalendarTitleForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.dateTimePicker_to.CustomFormat = "dd-MM-yyyy";
+            this.dateTimePicker_to.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker_to.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker_to.Location = new System.Drawing.Point(220, 109);
+            this.dateTimePicker_to.Name = "dateTimePicker_to";
+            this.dateTimePicker_to.Size = new System.Drawing.Size(178, 34);
+            this.dateTimePicker_to.TabIndex = 49;
+            // 
+            // Pic_createPDF
+            // 
+            this.Pic_createPDF.Image = global::Electricity_shop.Properties.Resources.doc;
+            this.Pic_createPDF.Location = new System.Drawing.Point(799, 599);
+            this.Pic_createPDF.Name = "Pic_createPDF";
+            this.Pic_createPDF.Size = new System.Drawing.Size(125, 133);
+            this.Pic_createPDF.TabIndex = 51;
+            this.Pic_createPDF.TabStop = false;
+            // 
             // Frm_documents
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
-            this.ClientSize = new System.Drawing.Size(1243, 683);
+            this.ClientSize = new System.Drawing.Size(952, 761);
+            this.Controls.Add(this.Pic_createPDF);
+            this.Controls.Add(this.dateTimePicker_to);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_exit);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.Btn_search);
-            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.dateTimePicker_from);
             this.Controls.Add(this.Lbl_info);
             this.Controls.Add(this.Lbl_inStockDocument);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Frm_documents";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "documents";
@@ -184,7 +244,11 @@ namespace Electricity_shop
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label Lbl_inStockDocument;
         private System.Windows.Forms.Label Lbl_info;
-        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_from;
         private System.Windows.Forms.Button Btn_search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_to;
+        private System.Windows.Forms.PictureBox Pic_createPDF;
     }
 }
